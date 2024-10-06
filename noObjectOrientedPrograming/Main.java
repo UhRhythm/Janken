c9fa8f63457ff3e95d4e24b86a8cae7068c8b877
UhRhythm <uhrhythm.0@gmail.com>
2024-10-07 00:20:48 +0900

    first commit

---
+++ noObjectOrientedPrograming/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Main");
+        final int STONE = 0;
+        final int SCISSORS = 1;
+        final int PAPER = 3; // staticを追加
+        System.out.println(PAPER);
+    }
+}
